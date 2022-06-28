8ee7f9bddee7ba3707ed619a075ba2d350666f59
Артем Римша <artem-rimsha@mail.ru>
2022-06-28 11:38:13 +0300

    Исправил ошибки, надеюсь)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -26,7 +26,8 @@ class Post(models.Model):
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        related_name="posts"
     )
 
     class Meta:
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
@@ -12,11 +12,9 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()
     context = {
         'group': group,
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
-# Create your views here.
